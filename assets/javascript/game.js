6fb5b1b34c99e137cee8d2a349426b5d01d190a9
Craig <corsi007@umn.edu>
2018-03-25 16:38:40 -0500

    set up currentSession object

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,69 @@
 // 'var phraseDictionary' and put the phrases here???
 
-var difficultyKey = {
-    'Easy': 10,
-    'Normal': 6,
-    'Hard': 4,
-    'Unlisted': 2
-}
-
-var difficultyLevel = difficultyKey['Normal'];
-
 var currentSession = {
     inGame: false,
     numberOfWins: 0,
     numberOfLosses: 0,
     difficulty: '',
-    phrase: '',
-    numberOfGuessesRemaining: difficultyLevel,
-    lettersAlreadyGuessed: [],
-    lettersToGuess: []
+    numberOfGuessesRemaining: 0,
+    phraseThisRound: '', // e.g. "be the change you want to see in the world"
+    phraseToGuess:'', // e.g. "b* th* chang* you want to s** in th* world"
+    phraseGuessed: '', // e.g. "-e --e -----e --- ---- -- -ee -- --e -----"
+   
+    lettersToFind: [], // e.g. ['b', 't', 'h', 'c', 'a', 'n', 'g', 'y', 'o', 'u', 'w', 's', 'i', 'r', 'l', 'd']
+    lettersAlreadyGuessed: [], // e.g. ['e']
+
+    difficultyKey: {
+        'Easy': 10,
+        'Normal': 6,
+        'Hard': 4,
+        'Unlisted': 2
+    },
+
+    startGame: function(difficulty) {
+        this.inGame = true;
+        this.difficulty = difficulty;
+        this.numberOfGuessesRemaining = this.difficultyKey[difficulty];
+
+        // find a random phrase / quote
+        this.phraseThisRound = "be the change you want to see in the world";
+
+        // invoke toPhrase to create a string of blank letters
+        this.phraseGuessed = '-- --- ------ --- ---- -- --- -- --- -----';
+
+        // set up lettersAlreadyGuessed and lettersToGuess
+        this.lettersAlreadyGuessed = [];
+        this.lettersToFind = ['b', 'e', 't', 'h', 'c', 'a', 'n', 'g', 'y', 'o', 'u', 'w', 's', 'i', 'r', 'l', 'd'];
+    
+        document.getElementById('displayStatus').innerHTML = 'You have selected the difficulty ' + difficulty;
+    },
+
+    endGame: function(result) {
+
+        this.inGame = false;
+        if (result) {
+            this.numberOfWins++;
+        } else {
+            this.numberOfLosses++;
+        }
+
+        this.difficulty = '',
+        this.numberOfGuessesRemaining = 0,
+        this.phraseThisRound = '',
+        this.phraseToGuess = '', 
+        this.phraseGuessed = '',
+        this.lettersToFind = [],
+        this.lettersAlreadyGuessed = []
+
+        document.getElementById('displayStatus').innerHTML = 'Hangman';
+    }
+}
+console.log(currentSession);
+
+document.getElementsByClassName('btn').onclick = function() {
+    console.log('Heyo');
+    console.log(this.innerHTML);
+    currentSession.startGame(this.innerHTML);
 }
 
 // initialize session
