55ea1d18cc7e052247f67d1a3fbe6b20dab81e0d
Craig <corsi007@umn.edu>
2018-03-24 23:55:04 -0500

    pseudocode game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,15 +10,32 @@ var difficultyLevel = difficultyKey['Normal'];
 var currentSession = {
     inGame: false,
     numberOfWins: 0,
-    numberOfLosses: 0
+    numberOfLosses: 0,
+    difficulty: '',
+    phrase: '',
+    numberOfGuessesRemaining: difficultyLevel,
+    lettersAlreadyGuessed: [],
+    lettersToGuess: []
 }
 
-class ThisGame {
-    constructor(difficulty, phrase) {
-        this.difficulty = difficulty;
-        this.phrase = phrase;
-    }
-    // numberOfGuessesRemaining: difficultyLevel,
-    // lettersAlreadyGuessed: []
-}
+// initialize session
+
+// initialize game after pressing any key to begin
+
+// display everything you need to know
+
+// listen for key presses...
+
+// update object properties accordingly
+
+
+// 'check status of game?' (Maybe as a method?)
+    // when numberOfGuessesRemaining hits zero, lose the game
+        // update losses, reset, enter between-games mode
+        // Change the HTML
+
+    // when length of lettersToGuess hits zero, win the game
+        // update wins, reset, enter between-games mode
+        // Change the HTML
+
 
